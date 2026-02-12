07e639b3d1e1bd6cd6cb9254e9abe3b0b402186d
Stanislaw <goral.stanislaw95@gmail.com>
2026-02-12 20:23:14 +0100

    added company type

---
+++ src/types/company.ts
@@ -0,0 +1,10 @@
+export interface Company {
+    id: number;
+    companyName: string;
+    industry: string;
+    email: string;
+    phone: string;
+    totalSales: number;
+    assignedEmployee: string;
+    createdAt: string;
+  }
